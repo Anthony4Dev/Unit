2c1194a9dfe36b14b1af7ba7a64da0c6fed01570
Anthony4dev <anthony.islan2005@gmail.com>
2023-10-21 17:52:44 -0300

    código já funcionando

--- Projeto/tests/CalcTest.php
+++ Projeto/tests/CalcTest.php
@@ -1,7 +1,30 @@
 <?php
 
+class Calculadora {
+    public function soma($a, $b) {
+        return $a + $b;
+    }
+
+    public function subtracao($a, $b) {
+        return $a - $b;
+    }
+
+    public function multiplicacao($a, $b) {
+        return $a * $b;
+    }
+
+    public function divisao($a, $b) {
+        if ($b == 0) {
+            throw new \InvalidArgumentException("Divisão por zero não é permitida.");
+        }
+        return $a / $b;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 use PHPUnit\Framework\TestCase;
-use Testando\Calculadora;
+
 
 class CalcTest extends TestCase {
     public function testSoma() {
